398d476f777b2a4e0e224fc4a1353efc0affb8ea
mvllow <mvllow@icloud.com>
2021-01-29 17:13:13 -0600

    refactor: xo all the things

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,13 +3,13 @@ import Section from '../components/section'
 import FakeList from '../components/fake-list'
 
 const Profile = () => (
-  <Page title='Profile'>
-    <Section>
-      <h2>Profile</h2>
-    </Section>
+	<Page title='Profile'>
+		<Section>
+			<h2>Profile</h2>
+		</Section>
 
-    <FakeList />
-  </Page>
+		<FakeList />
+	</Page>
 )
 
 export default Profile
--- src/theme.ts
+++ src/theme.ts
@@ -1,36 +1,37 @@
 declare global {
-  interface Window {
-    isLight: boolean
-  }
+	interface Window {
+		isLight: boolean
+	}
 }
 
 const isServer = typeof window === 'undefined'
 
 export function getLightMode() {
-  if (!isServer) {
-    return window.isLight
-  }
-  return false
+	if (!isServer) {
+		return window.isLight
+	}
+
+	return false
 }
 
 export function toggleLightMode() {
-  const isLight = getLightMode()
+	const isLight = getLightMode()
 
-  if (!isLight) {
-    try {
-      window.localStorage.setItem('light-mode', '1')
-      document.querySelector('html').classList.add('light')
-      window.isLight = true
-    } catch (err) {
-      // todo
-    }
-  } else {
-    try {
-      window.localStorage.removeItem('light-mode')
-      document.querySelector('html').classList.remove('light')
-      window.isLight = false
-    } catch (err) {
-      // todo
-    }
-  }
+	if (!isLight) {
+		try {
+			window.localStorage.setItem('light-mode', '1')
+			document.querySelector('html').classList.add('light')
+			window.isLight = true
+		} catch {
+			// Todo
+		}
+	} else {
+		try {
+			window.localStorage.removeItem('light-mode')
+			document.querySelector('html').classList.remove('light')
+			window.isLight = false
+		} catch {
+			// Todo
+		}
+	}
 }
