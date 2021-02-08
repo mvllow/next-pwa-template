a5975eecfd557314038f70f9f25cd9207da38b01
mvllow <mvllow@icloud.com>
2021-02-08 17:46:52 -0600

    lint: xo --fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,15 @@ import { ThemeProvider } from 'next-themes'
 import '@/styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider
+			attribute='class'
+			defaultTheme='system'
+			disableTransitionOnChange
+		>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
 
 export default App
