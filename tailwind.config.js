cac0e405a79d1eb3fb311ab44c5f62f77a9e9b29
not <mvllow@icloud.com>
2021-02-11 00:10:39 -0600

    chore(tailwind): remove unused .css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-	purge: ['./**/*.css', './{pages,components}/**/*.{js,ts,jsx,tsx}'],
+	purge: ['./{pages,components}/**/*.{js,ts,jsx,tsx}'],
 	darkMode: 'class',
 	theme: {
 		extend: {
