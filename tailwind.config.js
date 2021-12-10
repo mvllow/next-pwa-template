22511e91944d57e3a7ce6d25cd42dc3e009b44f6
mvllow <mvllow@icloud.com>
2021-12-10 13:08:23 -0600

    fix: styles rebuild in an infinite loop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-	content: ['./**/*.css', './{pages,components}/**/*.{js,ts,jsx,tsx}'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
 	darkMode: 'class',
 	plugins: [require('tailwindcss-safe-area')],
 }
