6488aaa535da83b166dd8126ef7842464f56861b
mvllow <mvllow@icloud.com>
2021-12-15 16:50:06 -0600

    fix: safe area and spacing in landscape mode

--- components/page.tsx
+++ components/page.tsx
@@ -22,9 +22,9 @@ const Page = ({ title, children }: Props) => (
 			 * Padding top = `appbar` height
 			 * Padding bottom = `bottom-nav` height
 			 */
-			className='mx-auto pt-20 pb-16 max-w-screen-md'
+			className='mx-auto px-safe pt-20 pb-16 sm:pb-0 max-w-screen-md'
 		>
-			<div className='mt-10 p-6'>{children}</div>
+			<div className='p-6'>{children}</div>
 		</main>
 
 		<BottomNav />
