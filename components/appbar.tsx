62a7a4d957346a802073a031165658b1c91b5d04
Nathan James <64075030+Nathanjms@users.noreply.github.com>
2023-11-14 16:15:08 +0000

    Fix for NextJS v13: Remove <a> tags inside of <Link> tags (#36)
    * Remove <a> tags inside of <Link> tags
    
    * Run `npx prettier --write ./components/{appbar,bottom-nav}.tsx`

--- components/appbar.tsx
+++ components/appbar.tsx
@@ -14,25 +14,23 @@ const Appbar = () => {
 			<header className='border-b bg-zinc-100 px-safe dark:border-zinc-800 dark:bg-zinc-900'>
 				<div className='mx-auto flex h-20 max-w-screen-md items-center justify-between px-6'>
 					<Link href='/'>
-						<a>
-							<h1 className='font-medium'>Rice Bowl</h1>
-						</a>
+						<h1 className='font-medium'>Rice Bowl</h1>
 					</Link>
 
 					<nav className='flex items-center space-x-6'>
 						<div className='hidden sm:block'>
 							<div className='flex items-center space-x-6'>
 								{links.map(({ label, href }) => (
-									<Link key={label} href={href}>
-										<a
-											className={`text-sm ${
-												router.pathname === href
-													? 'text-indigo-500 dark:text-indigo-400'
-													: 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
-											}`}
-										>
-											{label}
-										</a>
+									<Link
+										key={label}
+										href={href}
+										className={`text-sm ${
+											router.pathname === href
+												? 'text-indigo-500 dark:text-indigo-400'
+												: 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50'
+										}`}
+									>
+										{label}
 									</Link>
 								))}
 							</div>
